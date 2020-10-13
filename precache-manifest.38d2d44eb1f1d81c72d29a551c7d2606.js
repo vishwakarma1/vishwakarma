a555844e26192caea9ce43dd586083be4ab80637
nimupatel13 <patelnirmal13595@gmail.com>
2020-10-13 19:32:55 +0530

    Updates

---
+++ precache-manifest.38d2d44eb1f1d81c72d29a551c7d2606.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6939915df6defa327d39dfbf09cd7e33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bfc4b757773b7a54d75",
+    "url": "/static/css/main.33d47254.chunk.css"
+  },
+  {
+    "revision": "32399a2f73f2d5be10ab",
+    "url": "/static/js/2.43402301.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.43402301.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bfc4b757773b7a54d75",
+    "url": "/static/js/main.5a3f8e8a.chunk.js"
+  },
+  {
+    "revision": "756b43ce1f07a5f86e86",
+    "url": "/static/js/runtime-main.49d87ce1.js"
+  },
+  {
+    "revision": "1728c2e460d4fbe689ad6dec43d5a912",
+    "url": "/static/media/1.1728c2e4.jpg"
+  },
+  {
+    "revision": "f41f9f43e1dd9ca9c50bc9fd687b0864",
+    "url": "/static/media/10.f41f9f43.jpg"
+  },
+  {
+    "revision": "a7db227869216bdf874a5cb1884e9821",
+    "url": "/static/media/11.a7db2278.jpg"
+  },
+  {
+    "revision": "b3487b3385746a2ee0c32638c1872f6f",
+    "url": "/static/media/12.b3487b33.jpg"
+  },
+  {
+    "revision": "51af250751b093e06cc667909af73b65",
+    "url": "/static/media/2.51af2507.jpg"
+  },
+  {
+    "revision": "827bfccd90a4f4addc10335c978fcd98",
+    "url": "/static/media/3.827bfccd.jpg"
+  },
+  {
+    "revision": "7981cd33f40d0f01faf127cf56def0d9",
+    "url": "/static/media/4.7981cd33.jpg"
+  },
+  {
+    "revision": "26acc1a3f230ed9539b6c818813cf22e",
+    "url": "/static/media/5.26acc1a3.jpg"
+  },
+  {
+    "revision": "c467f6d8f69274d8c6b0a950eebfa965",
+    "url": "/static/media/6.c467f6d8.jpg"
+  },
+  {
+    "revision": "e0e69c84da1874afff4bc9b5ce6ea5fc",
+    "url": "/static/media/7.e0e69c84.jpg"
+  },
+  {
+    "revision": "745eb603d7f20a09aa787e77e087350a",
+    "url": "/static/media/8.745eb603.jpg"
+  },
+  {
+    "revision": "610ee470cf54383a7ae763792df6e2ea",
+    "url": "/static/media/9.610ee470.jpg"
+  },
+  {
+    "revision": "7c6a930db2581582da78ddef1a5c498a",
+    "url": "/static/media/perks.7c6a930d.jpg"
+  }
+]);
